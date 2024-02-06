e1b88516cbbe0e93c57dc890c8671cd1c173c370
thaiminhtuan135 <tuana11ht@gmail.com>
2024-02-06 17:27:11 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger(AppModule.name);
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(4242);
   logger.log(`server running on ${await app.getUrl()}`);
 }
